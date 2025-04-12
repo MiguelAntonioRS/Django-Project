869c55c4667614037d9ad6e348c6d904a907a172
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-04-12 12:41:00 -0400

    Created my first model

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    birth_date = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=8, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
