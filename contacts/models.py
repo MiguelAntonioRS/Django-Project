20746db551754e80763ee4be69add741de3000cf
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-04-14 21:31:09 -0400

    Creating a def function in the models.py

--- contacts/models.py
+++ contacts/models.py
@@ -8,4 +8,5 @@ class Contact(models.Model):
     phone = models.CharField(max_length=8, null=True, blank=True, verbose_name='Numero de telefono')
     created = models.DateTimeField(auto_now_add=True) 
     
-    
+    def __str__(self):
+        return self.name
