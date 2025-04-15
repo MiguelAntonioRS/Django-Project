39173a4a3ff8321cc9b1774ab7a817ca1ac6233e
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-04-14 21:52:33 -0400

    Creatin the first views

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from contacts.models import Contact
 
-# Create your views here.
+class ContactListView(generic, ListView):
+    model = Contact
