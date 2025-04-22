2bca628fb29f8cf2be335ab836ba919a79a2bc38
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-04-21 22:44:12 -0400

    Creating a new url for add contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from contacts import views
 
 urlpatterns = [
-    path('', views.ContactListView.as_view(), name='contact_list')
+    path('', views.ContactListView.as_view(), name='contact_list'),
+    path('add/', views.ContactCreateView.as_view(), name='contact_new'),
 ]
