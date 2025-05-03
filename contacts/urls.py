9bc5623d1ddeacf3f4e747ae074a84b64a0afb21
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-05-02 23:31:09 -0400

    Modified the archive .html

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,5 +4,5 @@ from contacts import views
 urlpatterns = [
     path('', views.ContactListView.as_view(), name='contact_list'),
     path('add/', views.ContactCreateView.as_view(), name='contact_new'),
-    path('<int:pk>/edit/', views.ContactUpdateView.as_view(), name='contact_new'),
+    path('<int:pk>/edit/', views.ContactUpdateView.as_view(), name='contact_edit'),
 ]
