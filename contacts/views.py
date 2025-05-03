39eeba162f81fc54c6782d0e73ba4169c249fe8c
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-05-02 23:24:22 -0400

    Creating the funcionality for update

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 from contacts.models import Contact
 
 class ContactListView(generic.ListView):
@@ -8,3 +9,8 @@ class ContactListView(generic.ListView):
 class ContactCreateView(generic.CreateView):
     model = Contact
     fields = ('name', 'email', 'birth_date', 'phone')
+    
+class ContactUpdateView(generic.UpdateView):
+    model = Contact
+    fields = ('name', 'email', 'birth_date', 'phone')
+    success_url = reverse_lazy('contact_list')
