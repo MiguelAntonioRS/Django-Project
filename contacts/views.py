ca879e44009f0fe69e98b1c6d0952a31bbbf5804
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-05-10 22:28:52 -0400

    Changes on the back

--- contacts/views.py
+++ contacts/views.py
@@ -9,6 +9,7 @@ class ContactListView(generic.ListView):
 class ContactCreateView(generic.CreateView):
     model = Contact
     fields = ('name', 'email', 'birth_date', 'phone')
+    success_url = reverse_lazy('contact_list')
     
 class ContactUpdateView(generic.UpdateView):
     model = Contact
