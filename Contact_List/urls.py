7b5eb5ce57f134e600fb3f3406fae8b7d008da52
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-08-22 11:47:09 -0400

    Modifying the urls

--- Contact_List/urls.py
+++ Contact_List/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Contact_List import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contacts.urls')),
 ]
+
+if settings.Debug:
+    urlpatterns = static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
