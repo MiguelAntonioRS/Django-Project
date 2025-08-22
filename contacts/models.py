d8f789e15616b8af9d1bd0cc95377e7e9251eadf
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-08-22 11:40:18 -0400

    Creating the image model and redirecting the root for the image

--- contacts/models.py
+++ contacts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Contact(models.Model):
     
+    avatar = models.ImageField(null=True, blank=True, upload_to='contact')
     name = models.CharField(max_length=100, verbose_name='Nombre')
     email = models.EmailField(max_length=50)
     birth_date = models.DateField(null=True, blank=True, verbose_name='Fecha de Nacimiento')
