2fa844c864803ec116707a93d7cfc5d4e42accab
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-09-02 11:48:30 -0400

    Modifing settings.py

--- Contact_List/settings.py
+++ Contact_List/settings.py
@@ -130,3 +130,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
